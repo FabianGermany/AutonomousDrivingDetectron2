a96f656dc59eac2be9591e969f524432ff31e20d
Fabian Lüttel <fabianluettel@web.de>
2021-06-09 11:30:30 +0200

    add functions.py

--- README.md
+++ README.md
@@ -5,8 +5,8 @@ Object detection system that can be used for autonomous vehicles
 ![Screenshot](preview_files/example_output.jpg)
 
 For this project, I use Detectron2 from Facebook (https://github.com/facebookresearch/detectron2) to train and test data
-from Audi (https://www.a2d2.audi/a2d2/en.html). For citations have a look on my thesis.
+from Audi (https://www.a2d2.audi/a2d2/en.html). For further citations have a look on my thesis.
 
-Due to the required GPU capacities, this project is embedded into a Google-Colab notebook so that the training and testing of the model can be outsourced to a Amazon AWS Server. The notebook is based on the example notebook from Facebook's Detectron2 (https://colab.research.google.com/drive/16jcaJoc6bCFAQ96jDe2HwtXj7BMD_-m5).
+Due to the required GPU capacities, this project is embedded into a Google-Colab notebook so that the training and testing of the model can be outsourced to a Amazon AWS Server. The notebook is based on the example notebook from Facebook's Detectron2 (https://colab.research.google.com/drive/16jcaJoc6bCFAQ96jDe2HwtXj7BMD_-m5). Documentation about the Audi A2D2 dataset can be found here: https://www.a2d2.audi/a2d2/en/tutorial.html
 
 Consequently, the heart of this project is the Google-Colab notebook file. It can be run locally, on a Google-Colab server or better on a high performance server such as Amazon AWS S3 SageMaker.
---
+++ functions.py
@@ -0,0 +1,120 @@
+#Functions needed to handle the Audi A2D2 dataset, according to https://www.a2d2.audi/a2d2/en/tutorial.html
+
+import json
+import pprint
+import numpy as np
+import numpy.linalg as la
+import open3d as o3
+
+def skew_sym_matrix(u):
+    return np.array([[    0, -u[2],  u[1]], 
+                     [ u[2],     0, -u[0]], 
+                     [-u[1],  u[0],    0]])
+
+
+def axis_angle_to_rotation_mat(axis, angle):
+    return np.cos(angle) * np.eye(3) + \
+        np.sin(angle) * skew_sym_matrix(axis) + \
+        (1 - np.cos(angle)) * np.outer(axis, axis)
+
+
+def read_bounding_boxes(file_name_bboxes):
+    # open the file
+    with open (file_name_bboxes, 'r') as f:
+        bboxes = json.load(f)
+        
+    boxes = [] # a list for containing bounding boxes  
+    print(bboxes.keys())
+    
+    for bbox in bboxes.keys():
+        bbox_read = {} # a dictionary for a given bounding box
+        bbox_read['class'] = bboxes[bbox]['class']
+        bbox_read['truncation']= bboxes[bbox]['truncation']
+        bbox_read['occlusion']= bboxes[bbox]['occlusion']
+        bbox_read['alpha']= bboxes[bbox]['alpha']
+        bbox_read['top'] = bboxes[bbox]['2d_bbox'][0]
+        bbox_read['left'] = bboxes[bbox]['2d_bbox'][1]
+        bbox_read['bottom'] = bboxes[bbox]['2d_bbox'][2]
+        bbox_read['right']= bboxes[bbox]['2d_bbox'][3]
+        bbox_read['center'] =  np.array(bboxes[bbox]['center'])
+        bbox_read['size'] =  np.array(bboxes[bbox]['size'])
+        angle = bboxes[bbox]['rot_angle']
+        axis = np.array(bboxes[bbox]['axis'])
+        bbox_read['rotation'] = axis_angle_to_rotation_mat(axis, angle) 
+        boxes.append(bbox_read)
+
+    return boxes 
+
+
+def extract_bboxes_file_name_from_image_file_name(file_name_image):
+    file_name_bboxes = file_name_image.split('/')
+    file_name_bboxes = file_name_bboxes[-1].split('.')[0]
+    file_name_bboxes = file_name_bboxes.split('_')
+    file_name_bboxes = file_name_bboxes[0] + '_' + \
+                  'label3D_' + \
+                  file_name_bboxes[2] + '_' + \
+                  file_name_bboxes[3] + '.json'
+    
+    return file_name_bboxes
+
+
+def get_points(bbox):
+    half_size = bbox['size'] / 2.
+    
+    if half_size[0] > 0:
+        # calculate unrotated corner point offsets relative to center
+        brl = np.asarray([-half_size[0], +half_size[1], -half_size[2]])
+        bfl = np.asarray([+half_size[0], +half_size[1], -half_size[2]])
+        bfr = np.asarray([+half_size[0], -half_size[1], -half_size[2]])
+        brr = np.asarray([-half_size[0], -half_size[1], -half_size[2]])
+        trl = np.asarray([-half_size[0], +half_size[1], +half_size[2]])
+        tfl = np.asarray([+half_size[0], +half_size[1], +half_size[2]])
+        tfr = np.asarray([+half_size[0], -half_size[1], +half_size[2]])
+        trr = np.asarray([-half_size[0], -half_size[1], +half_size[2]])
+     
+        # rotate points
+        points = np.asarray([brl, bfl, bfr, brr, trl, tfl, tfr, trr])
+        points = np.dot(points, bbox['rotation'].T)
+        
+        # add center position
+        points = points + bbox['center']
+  
+    return points
+
+
+# Create or update open3d wire frame geometry for the given bounding boxes
+def _get_bboxes_wire_frames(bboxes, linesets=None, color=None):
+
+    num_boxes = len(bboxes)
+        
+    # initialize linesets, if not given
+    if linesets is None:
+        linesets = [o3.geometry.LineSet() for _ in range(num_boxes)]
+
+    # set default color
+    if color is None:
+        #color = [1, 0, 0]
+        color = [0, 0, 1]
+
+    assert len(linesets) == num_boxes, "Number of linesets must equal number of bounding boxes"
+
+    # point indices defining bounding box edges
+    lines = [[0, 1], [1, 2], [2, 3], [3, 0],
+             [0, 4], [1, 5], [2, 6], [3, 7],
+             [4, 5], [5, 6], [6, 7], [7, 4], 
+             [5, 2], [1, 6]]
+
+    # loop over all bounding boxes
+    for i in range(num_boxes):
+        # get bounding box corner points
+        points = get_points(bboxes[i])
+        # update corresponding Open3d line set
+        colors = [color for _ in range(len(lines))]
+        line_set = linesets[i]
+        line_set.points = o3.utility.Vector3dVector(points)
+        line_set.lines = o3.utility.Vector2iVector(lines)
+        line_set.colors = o3.utility.Vector3dVector(colors)
+
+    return linesets
+
+print("Function import done.")
